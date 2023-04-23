9a5e12be8ca197f12e3d27ad75321d031699191f
jenish Dhanani <99007146+Jenish-Dhanani@users.noreply.github.com>
2023-04-23 16:03:17 +0000

    Added Pagination

--- src/store.js
+++ src/store.js
@@ -18,17 +18,20 @@ const usePokemonSource = () => {
     }
   );
 
-  const [{ search }, dispatch] = useReducer(
+  const [{ search, page }, dispatch] = useReducer(
     (state, action) => {
       switch (action.type) {
         case "SET_SEARCH":
-          return { ...state, search: action.payload };
+          return { ...state, search: action.payload, page: 0 };
+        case "SET_PAGE":
+          return { ...state, page: action.payload };
         default:
           return state;
       }
     },
     {
       search: "",
+      page: 1,
     }
   );
 
@@ -39,19 +42,34 @@ const usePokemonSource = () => {
     });
   }, []);
 
+  const setPage = useCallback((page) => {
+    dispatch({
+      type: "SET_PAGE",
+      payload: page,
+    });
+  }, []);
+
   const filteredPokemon = useMemo(
     () =>
       pokemon
-        .filter((p) => p.name.toLowerCase().includes(search.toLowerCase()))
-        .slice(0, 20),
-    [pokemon, search]
+        .filter((p) => p.name.toLowerCase().includes(search.toLowerCase())),
+        // .slice(page * 20, (page + 1) * 20),
+    [pokemon, search, page]
   );
 
   const sortedPokemon = useMemo(
     () => [...filteredPokemon].sort((a, b) => a.name.localeCompare(b.name)),
     [filteredPokemon]
   );
-  return { pokemon: sortedPokemon, search, setSearch };
+
+  const paginatedPokemon = useMemo(
+    ()=> [...sortedPokemon].slice(page * 20 - 20, page * 20),
+    [sortedPokemon]
+  )
+
+  const totalPages = useMemo(() => Math.floor(pokemon.length / 20), [pokemon]);
+
+  return { pokemon: paginatedPokemon, search, setSearch, page, setPage, totalPages };
 };
 
 const pokemonContext = createContext();
